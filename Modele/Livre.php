d26c3fcea81b4238f84b7f9148cdb2b77cecfa05
oceane tardy <tardy.oceane42@gmail.com>
2023-06-20 13:40:49 +0200

    [+] Ajout début Modele + connexion bdd

---
+++ Modele/Livre.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'Modele/Modele.php';
+
+
+class Livre extends Modele {
+
+
+    public function getLivres() {
+        $sql = 'SELECT l.id, l.titre, l.isbn, a.nom, a.prenom
+        FROM livre l
+        LEFT JOIN `auteur`a ON a.`id` = l.`ref_auteur`';
+
+        //Ajouter requete
+        $livres = $this->executerRequete($sql);
+        return $livres;
+    }
+
+
+    public function getLivre($idLivre) {
+        $sql = 'SELECT l.id, l.titre, l.isbn, a.nom, a.prenom
+        FROM livre l
+        LEFT JOIN `auteur`a ON a.`id` = l.`ref_auteur`
+        WHERE l.`idLivre` = '. $idLivre;
+
+        $livres = $this->executerRequete($sql);
+        return $livres;
+        $livre = $this->executerRequete($sql, array($idLivre));
+        if ($livre->rowCount() > 0)
+            return $livre->fetch(); 
+        else
+            throw new Exception("Aucun livre ne correspond à l'identifiant '$idLivre'");
+    }
+
+}
---
+++ Modele/Modele.php
@@ -0,0 +1,28 @@
+<?php
+
+abstract class Modele {
+
+    private $bdd;
+
+
+    protected function executerRequete($sql, $params = null) {
+        if ($params == null) {
+            $resultat = $this->getBdd()->query($sql);
+        }
+        else {
+            $resultat = $this->getBdd()->prepare($sql);
+            $resultat->execute($params);
+        }
+        return $resultat;
+    }
+
+    private function getBdd() {
+        if ($this->bdd == null) {
+            $this->bdd = new PDO('mysql:host=host.docker.internal:3306;dbname=bibliotheque;charset=utf8',
+                    'root', '',
+                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        return $this->bdd;
+    }
+
+}
